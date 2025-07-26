f99fd49955479b218f411ce1f8f7df4b689ed665
Андрей Лукашук <andriy2006lukashuk@gmail.com>
2025-07-26 13:36:34 +0200

    finish main

---
+++ src/server.js
@@ -0,0 +1,65 @@
+import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+
+import { getEnvVar } from './utils/getEnvVar.js';
+import { getAllContacts, getContactById } from './services/contacts.js';
+
+const PORT = getEnvVar('PORT');
+
+export const setupServer = () => {
+  const app = express();
+
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+  app.use(cors());
+
+  app.get('/', (req, res) => {
+    res.json({
+      message: 'This is my Contact App',
+    });
+  });
+
+  app.get('/contacts', async (req, res) => {
+    const contacts = await getAllContacts();
+    res.json({
+      status: 200,
+      message: 'Successfully found contacts!',
+      data: contacts,
+    });
+  });
+
+  app.get('/contacts/:id', async (req, res) => {
+    const { id } = req.params;
+    const contact = await getContactById(id);
+    if (!contact) {
+      res.status(404).json({
+        message: 'Contact not found',
+      });
+      return;
+    }
+    res.json({
+      status: 200,
+      message: `Successfully found contact with id: ${id}!`,
+      data: contact,
+    });
+  });
+
+  app.use('', (req, res) => {
+    res.status(404).json({
+      message: 'Not found',
+    });
+  });
+
+  app.listen(PORT, (error) => {
+    if (error) {
+      throw new Error(error);
+    }
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
