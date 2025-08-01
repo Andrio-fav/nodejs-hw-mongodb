9d3453b8e1a819d2788b17243b3bc91868ab1b33
Андрей Лукашук <andriy2006lukashuk@gmail.com>
2025-08-01 22:14:36 +0200

    lets go

--- src/server.js
+++ src/server.js
@@ -1,19 +1,27 @@
-import 'dotenv/config'; 
 import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
 
 import { getEnvVar } from './utils/getEnvVar.js';
-import { getAllContacts, getContactById } from './services/contacts.js';
 
+import contactsRouter from './routes/contacts.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import { errorHandler } from './middlewares/errorHandler.js';
 
+const PORT = getEnvVar('PORT');
 
 export const setupServer = () => {
-  const PORT = getEnvVar('PORT');
-  
   const app = express();
 
-  app.use(pino());
+  app.use(express.json());
+
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
   app.use(cors());
 
   app.get('/', (req, res) => {
@@ -21,49 +29,10 @@ export const setupServer = () => {
       message: 'This is my Contact App',
     });
   });
+  app.use('/contacts', contactsRouter);
 
-  app.get('/contacts', async (req, res) => {
-    try {
-      const contacts = await getAllContacts();
-      res.json({
-        status: 200,
-        message: 'Successfully found contacts!',
-        data: contacts,
-      });
-    } catch (error) {
-      res.status(500).json({
-        message: 'Server error',
-      });
-    }
-  });
-
-  app.get('/contacts/:id', async (req, res) => {
-    try {
-      const { id } = req.params;
-      const contact = await getContactById(id);
-      if (!contact) {
-        return res.status(404).json({
-          message: 'Contact not found',
-        });
-      }
-      res.json({
-        status: 200,
-        message: `Successfully found contact with id: ${id}!`,
-        data: contact,
-      });
-    } catch (error) {
-      res.status(500).json({
-        message: 'Server error',
-      });
-    }
-  });
-
-  
-  app.use('', (req, res) => {
-    res.status(404).json({
-      message: 'Not found',
-    });
-  });
+  app.use('', notFoundHandler);
+  app.use(errorHandler);
 
   app.listen(PORT, (error) => {
     if (error) {
