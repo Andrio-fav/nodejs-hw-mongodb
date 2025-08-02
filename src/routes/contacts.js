ebc72d4e64230e2bce136a32d71a1b2baf506be0
Андрей Лукашук <andriy2006lukashuk@gmail.com>
2025-08-02 16:14:56 +0200

    next

--- src/routes/contacts.js
+++ src/routes/contacts.js
@@ -11,9 +11,9 @@ import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 
 const router = Router();
 
-router.get('/contacts', ctrlWrapper(getContactsController));
+router.get('/', ctrlWrapper(getContactsController));
 router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
-router.post('/contacts', ctrlWrapper(createContactController));
+router.post('/', ctrlWrapper(createContactController));
 router.patch('/contacts/:contactId', ctrlWrapper(updateContactController));
 router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
 
