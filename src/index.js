0e3a14f6d5aa1cd151a8951a00cc826cdfa0c1a3
Андрей Лукашук <andriy2006lukashuk@gmail.com>
2025-08-15 20:11:15 +0200

    change

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import { initMongoDB } from './db/initMongoConnection.js';
+import { initMongoConnection } from './db/initMongoConnection.js';
 import { startServer } from './server.js';
 import { createDirIfNotExists } from './utils/createDirlfNotExists.js';
 import { TEMP_UPLOAD_DIR, UPLOAD_DIR } from './constants/index.js';
 
 const bootstrap = async () => {
-  await initMongoDB();
+  await initMongoConnection();
   await createDirIfNotExists(TEMP_UPLOAD_DIR);
   await createDirIfNotExists(UPLOAD_DIR);
   startServer();
