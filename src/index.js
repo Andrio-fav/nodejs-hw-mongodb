f702655f091eebcae858f484b231950cea56ae70
Андрей Лукашук <andriy2006lukashuk@gmail.com>
2025-08-15 20:03:13 +0200

    change

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { initMongoDB } from './db/initMongoDB.js';
+import { initMongoDB } from './db/initMongoConnection.js';
 import { startServer } from './server.js';
 import { createDirIfNotExists } from './utils/createDirIfNotExists.js';
 import { TEMP_UPLOAD_DIR, UPLOAD_DIR } from './constants/index.js';
