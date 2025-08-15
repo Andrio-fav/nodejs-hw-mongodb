cc18b3c754b27617d833e910ee344c7bf8fe9dc2
Андрей Лукашук <andriy2006lukashuk@gmail.com>
2025-08-15 11:31:34 +0200

    restart

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -57,7 +57,7 @@ export const createContactController = async (req, res) => {
 
   res.status(201).json({
     status: 201,
-    message: `Successfully created a contact!`,
+    message: 'Successfully created a contact!',
     data: contact,
   });
 };
