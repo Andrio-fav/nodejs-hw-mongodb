b21bae36c35b598556196e24471c944711937805
Андрей Лукашук <andriy2006lukashuk@gmail.com>
2025-08-15 10:23:29 +0200

    restart

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,8 +1,8 @@
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import createHttpError from 'http-errors';
-import User from '../models/user.js';
-import Session from '../models/session.js';
+import User from '../db/models/user.js';
+import Session from '../db/models/session.js';
 import { getEnvVar } from '../utils/getEnvVar.js';
 
 const JWT_SECRET = getEnvVar('JWT_SECRET');
