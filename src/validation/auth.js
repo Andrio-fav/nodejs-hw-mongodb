f052c25ecfd71f73d604d0d7a3ea5416e2609e80
Андрей Лукашук <andriy2006lukashuk@gmail.com>
2025-08-15 22:37:25 +0200

    gogogo

--- src/validation/auth.js
+++ src/validation/auth.js
@@ -3,8 +3,8 @@ import Joi from 'joi';
 export const registerUserSchema = Joi.object({
   name: Joi.string().min(3).max(20).required().messages({
     'string.base': 'Username should be a string',
-    'string.min': 'Username should have at least 3 charachters',
-    'string.max': 'Username should have not more than 20 charachters',
+    'string.min': 'Username should have at least 3 characters',
+    'string.max': 'Username should have not more than 20 characters',
     'any.required': 'Username is required',
   }),
   email: Joi.string().email().required().messages({
@@ -30,11 +30,20 @@ export const loginUserSchema = Joi.object({
   }),
 });
 
-export const requestResetPwdEmailSchema = JoiValidation.object({
-  email: JoiValidation.string().email().required(),
+export const requestResetPwdEmailSchema = Joi.object({
+  email: Joi.string().email().required().messages({
+    'string.base': 'Email should be a string',
+    'any.required': 'Email is required',
+  }),
 });
 
-export const resetAuthPasswordSchema = JoiValidation.object({
-  password: JoiValidation.string().required(),
-  token: JoiValidation.string().required(),
+export const resetAuthPasswordSchema = Joi.object({
+  password: Joi.string().required().messages({
+    'string.base': 'Password should be a string',
+    'any.required': 'Password is required',
+  }),
+  token: Joi.string().required().messages({
+    'string.base': 'Token should be a string',
+    'any.required': 'Token is required',
+  }),
 });
