654e6e446854ccedc6f751559ff6024963bb4720
Andrio-fav <andriy2006lukashuk@gmail.com>
2025-08-16 13:57:44 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import { initMongoConnection } from './db/initMongoConnection.js';
 import { setupServer } from './server.js';
-import { createDirIfNotExists } from './utils/createDirlfNotExists.js';
-import { TEMP_UPLOAD_DIR, UPLOAD_DIR } from './constants/index.js';
 
-const bootstrap = async () => {
+const startServer = async () => {
   await initMongoConnection();
-  await createDirIfNotExists(TEMP_UPLOAD_DIR);
-  await createDirIfNotExists(UPLOAD_DIR);
   setupServer();
 };
 
-void bootstrap();
+startServer();
